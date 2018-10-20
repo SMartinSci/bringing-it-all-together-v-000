b9a46ce8dc4e327ebf726bb4f6d24b0fd61a5940
Sherry Martin <sm674@cornell.edu>
2018-10-20 17:28:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,6 +8,15 @@ def initialize(:id, :name, :breed)
   @breed = breed
 end
 
-
+def self.find_or_create_by(name:, breed:)
+    song = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !song.empty?
+      dog_data = song[0]
+      dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
+    else
+      song = self.create(name: name, album: album)
+    end
+    song
+  end 
 
 end
