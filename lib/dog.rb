55723948aaf00762bbca1233627c3dd72d676798
Sherry Martin <sm674@cornell.edu>
2018-10-20 17:46:32 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,13 +3,13 @@ require_relative "../config/environment.rb"
 class Dog
   attr_accessor :name, :breed, :id
 
-def initialize(id: nil, name:, breed:)
+  def initialize(id: nil, name:, breed:)
   @id = id
   @name = name
   @breed = breed
 end
 
-def self.create_table
+  def self.create_table
     sql =  <<-SQL
       CREATE TABLE IF NOT EXISTS dogs (
         id INTEGER PRIMARY KEY,
@@ -38,9 +38,9 @@ def self.create_table
   end
 
   def self.drop_table
-      sql = "DROP TABLE IF EXISTS dogs"
-      DB[:conn].execute(sql)
-    end
+    sql = "DROP TABLE IF EXISTS dogs"
+    DB[:conn].execute(sql)
+  end
 
   def self.new_from_db(row)
       id = row[0]
