e2bad31280314a1417be4d31da8642db0b08ec02
Sherry Martin <sm674@cornell.edu>
2018-10-20 17:47:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -77,21 +77,21 @@ end
   end
 
   def self.find_by_id(id)
-  sql = <<-SQL
-    SELECT *
-    FROM dogs
-    WHERE id = ?
-    LIMIT 1
-  SQL
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE id = ?
+      LIMIT 1
+    SQL
 
-  DB[:conn].execute(sql,id).map do |row|
-    self.new_from_db(row)
-  end.first
-end
+    DB[:conn].execute(sql,id).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 
   def update
     sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
-  
+
 end
