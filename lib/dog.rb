3b27392e112def10a2436ac81d75fb51d9e9024b
Sherry Martin <sm674@cornell.edu>
2018-10-20 17:36:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -20,6 +20,13 @@ def self.find_or_create_by(name:, breed:)
     dog
   end
 
+  def self.new_from_db(row)
+      id = row[0]
+      name =  row[1]
+      grade = row[2]
+      self.new(id, name, breed)
+  end
+
   def self.find_by_name(name)
       sql = "SELECT * FROM dogs WHERE name = ?"
       result = DB[:conn].execute(sql, name)[0]
