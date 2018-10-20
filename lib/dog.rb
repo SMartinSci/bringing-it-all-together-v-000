421c7b2556e20dec3a4b6ee1af661bd977d3d62d
Sherry Martin <sm674@cornell.edu>
2018-10-20 17:34:21 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -20,4 +20,29 @@ def self.find_or_create_by(name:, breed:)
     dog
   end
 
+  def self.find_by_name(name)
+      sql = "SELECT * FROM dogs WHERE name = ?"
+      result = DB[:conn].execute(sql, name)[0]
+      Dog.new(result[0], result[1], result[2])
+    end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, album = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
+
+  def save
+    if self.id 
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed) 
+        VALUES (?, ?)
+      SQL
+ 
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+  end
 end
